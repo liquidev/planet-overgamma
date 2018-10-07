117867df02ff1ce8b586d7e4a45655e86d336aac
lqdev <liquidekgaming@gmail.com>
2018-10-08 01:00:47 +0200

    Cover

--- scripts/dist.sh
+++ scripts/dist.sh
@@ -1,7 +1,13 @@
+#!/bin/bash
+
 echo preparing for distribution
+rm -rf dist
 mkdir dist
 echo love archive
 zip -9 -r dist/spamality.love .
 echo windows executable
-cat dist/love.exe dist/spamality.love > dist/spamality.exe
+cat love/love.exe dist/spamality.love > dist/spamality.exe
+cp love/* dist
+rm dist/love.exe
+rm dist/lovec.exe
 echo done

8e1b3ac8fc58b38b2af1a3f9f95e55ddbfa0f3e3
liquid600pgm <liquidekgaming@gmail.com>
2019-05-08 16:22:24 +0200

    Resources

---
+++ planet_overgamma.nimble
@@ -0,0 +1,13 @@
+# Package
+
+version       = "0.1.0"
+author        = "liquid600pgm"
+description   = "You crash on an irradiated, overgrown planet. Use modular machines to find your way back home"
+license       = "GPL-2.0"
+srcDir        = "src"
+bin           = @["planet_overgamma"]
+
+# Dependencies
+
+requires "nim >= 0.19.9"
+requires "rapid"
---
+++ src/planet_overgamma.nim
@@ -0,0 +1,27 @@
+#--
+# Planet Overgamma
+# a game about planets, machines, and robots.
+# copyright (C) iLiquid, 2018-2019
+#--
+
+import rapid/gfx/surface
+
+import sharedres
+
+proc main() =
+  var
+    win = initRWindow()
+      .size(1024, 768)
+      .title("Planet Overgamma – alpha")
+      .open()
+    gfx = win.openGfx()
+
+  load()
+
+  gfx.loop:
+    draw ctx, step:
+      discard
+    update step:
+      discard
+
+when isMainModule: main()
